dd207b433553534202f646f01c03475b348b760d
Sparrow <dfleites1234@gmail.com>
2022-09-27 01:03:16 -0500

    Coding Challenge #3 setup

--- starter/script.js
+++ starter/script.js
@@ -48,6 +48,23 @@ const restaurant = {
   },
 };
 
+//////////////////////////////////////////////////////////////////////////////////////////////////
+// Coding Challenge #3
+
+const gameEvents = new Map([
+  [17, 'GOAL'],
+  [36, 'Substitution'],
+  [47, 'GOAL'],
+  [61, 'Substitution'],
+  [64, 'Yellow card'],
+  [69, 'Red card'],
+  [70, 'Substitution'],
+  [72, 'Substitution'],
+  [72, 'GOAL'],
+  [80, 'GOAL'],
+  [92, 'Yellow card'],
+]);
+
 /*
 //////////////////////////////////////////////////////////////////////////////////////////////////
 // Maps: Iteration
