0e90b00c2ce52878c71fa4fe38b9a1bef74aa055
Sparrow <dfleites1234@gmail.com>
2022-09-28 18:19:55 -0500

    Working with Strings- Part 2

--- starter/script.js
+++ starter/script.js
@@ -49,6 +49,69 @@ const restaurant = {
 };
 
 //////////////////////////////////////////////////////////////////////////////////////////////////
+// Working with Strings- Part 2
+
+const airline = 'TAP Air Portugal';
+
+console.log(airline.toLowerCase());
+console.log(airline.toUpperCase());
+
+// Fix capitalization in name
+const passenger = 'jOnAS'; // Jonas
+const passengerLower = passenger.toLowerCase();
+const passengerCorrect =
+  passengerLower[0].toUpperCase() + passengerLower.slice(1);
+console.log(passengerCorrect);
+
+// Comparing emails
+const email = 'hello@jonas.io';
+const loginEmail = '  Hello@Jonas.Io \n';
+
+// const lowerEmail = loginEmail.toLowerCase();
+// const trimmedEmail = lowerEmail.trim();
+const normalizedEmail = loginEmail.toLowerCase().trim();
+console.log(normalizedEmail);
+console.log(email === normalizedEmail);
+
+// replacing parts of strings
+const priceGB = '288,97£';
+const priceUS = priceGB.replace('£', '$').replace(',', '.');
+console.log(priceUS);
+
+const announcement =
+  'All passengers come to boarding door 23. Boarding door 23!';
+console.log(announcement.replace('door', 'gate'));
+// using replaceAll changes all of the occurences
+// console.log(announcement.replaceAll('door', 'gate'));
+
+console.log(announcement.replace(/door/g, 'gate'));
+
+// Booleans
+const plane = 'Airbus A320neo';
+console.log(plane.includes('A320'));
+console.log(plane.includes('Boeing'));
+console.log(plane.startsWith('Airb'));
+
+if (plane.startsWith('Airbus') && plane.endsWith('neo')) {
+  console.log('Part of the NEW Airbus family');
+}
+
+// Practice exercise
+const checkBaggage = function (items) {
+  const baggage = items.toLowerCase();
+
+  if (baggage.includes('knife') || baggage.includes('gun')) {
+    console.log('You are NOT allowed on board');
+  } else {
+    console.log('Welcome aboard!');
+  }
+};
+checkBaggage('I have a laptop, some Food and a pocket Knife');
+checkBaggage('Socks and a camera');
+checkBaggage('Got some snacks and a gun for protection');
+
+/*
+//////////////////////////////////////////////////////////////////////////////////////////////////
 // Working with Strings- Part 1
 
 const airline = 'TAP Air Portugal';
@@ -91,7 +154,7 @@ console.log(typeof new String('jonas')); // Shows 'Object'
 
 console.log(typeof new String('jonas').slice(1)); // Shows 'String'
 
-/*
+
 //////////////////////////////////////////////////////////////////////////////////////////////////
 // Coding Challenge #3
 
