b574e3d31de153dfafcd32c7418894ef2a9115a8
Sparrow <dfleites1234@gmail.com>
2022-09-30 14:11:10 -0500

    Coding Challenge #4 continued

--- starter/script.js
+++ starter/script.js
@@ -52,20 +52,38 @@ const restaurant = {
 // Coding Challenge #4
 
 // Test Data:
-// underscore_case;
-// first_name;
-// Some_Variable;
-// calculate_AGE;
-// delayed_departure;
+// underscore_case
+// first_name
+// Some_Variable
+// calculate_AGE
+// delayed_departure
 
 document.body.append(document.createElement('textarea'));
 document.body.append(document.createElement('button'));
-const text = document.querySelector('textarea').value;
+// const text = document.querySelector('textarea').value;
 
 document.querySelector('button').addEventListener('click', function () {
   const text = document.querySelector('textarea').value;
-  console.log(text);
+  const rows = text.split('\n');
+  // console.log(rows);
+
+  for (const row of rows) {
+    const [first, second] = row.toLowerCase().trim().split('_');
+    console.log(row, first, second);
+    const output = `${first}${second.replace(
+      second[0],
+      second[0].toUpperCase()
+    )}`;
+    console.log(output);
+  }
 });
+
+// underscore_case -> underscoreCase
+// underscore_case
+// first_name
+// Some_Variable
+// calculate_AGE
+// delayed_departure
 
 /*
 //////////////////////////////////////////////////////////////////////////////////////////////////
